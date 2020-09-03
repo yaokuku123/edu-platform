fe335f366bab627f2b57d8646138191793836ceb
yaokuku123 <yaoqijun@outlook.com>
2020-09-03 19:23:09 +0800

    小节信息的CRUD接口

--- service/service_edu/src/main/java/com/yqj/serviceedu/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/yqj/serviceedu/controller/EduVideoController.java
@@ -1,9 +1,11 @@
 package com.yqj.serviceedu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.yqj.commonutils.R;
+import com.yqj.serviceedu.entity.EduVideo;
+import com.yqj.serviceedu.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +16,39 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-09-01
  */
 @RestController
-@RequestMapping("/serviceedu/edu-video")
+@RequestMapping("/serviceedu/video")
+@CrossOrigin
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService videoService;
+
+    //增加小节信息
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+
+    //根据id查询小节信息
+    @GetMapping("getVideoInfo/{videoId}")
+    public R getVideoInfo(@PathVariable String videoId){
+        EduVideo eduVideo = videoService.getById(videoId);
+        return R.ok().data("video",eduVideo);
+    }
+
+    //修改小节信息
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        videoService.updateById(eduVideo);
+        return R.ok();
+    }
+
+    //删除小节信息
+    @DeleteMapping("{videoId}")
+    public R deleteVideo(@PathVariable String videoId){
+        videoService.removeById(videoId);
+        return R.ok();
+    }
 }
 
